cce7312284a4f6559656a3a6468798c08a52e04e
LoneCannibal <69956836+LoneCannibal@users.noreply.github.com>
2020-12-14 15:55:09 +0530

    Added user detials window

--- SampleController.java
+++ SampleController.java
@@ -5,11 +5,14 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.*;
+
+import java.awt.event.ItemEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -21,7 +24,9 @@ public class SampleController {
     public JFXTextField email_create;
     public JFXPasswordField password_create;
     public JFXPasswordField password2_create;
-
+    public JFXTextField username;
+    public JFXPasswordField password;
+    public Label wrong_password;
 
 
     public void exit(ActionEvent ae)
@@ -32,34 +37,78 @@ public class SampleController {
     {
         create();
     }
-    public void save_to_db(ActionEvent ae)
+
+    public void login(ActionEvent ae)
     {
-        try
-        {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/mini_project","root","password");
-            String sql = "INSERT INTO `user` (`name`, `email`, `password`,`username`) VALUES ( ?, ?, ?,?);";
-            PreparedStatement pr = conn.prepareStatement(sql);
-            pr.setString(1,name_create.getText());
-            pr.setString(2,email_create.getText());
-            pr.setString(3,password_create.getText());
-            pr.setString(4,username_create.getText());
-            pr.executeUpdate();
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mini_project", "root", "password");
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT `password` FROM `user` WHERE `password` LIKE ";
+            sql=sql+"'"+ password.getText()+"' AND `username` LIKE '"+username.getText()+"'";
+            ResultSet rs = stmt.executeQuery(sql);
+            if(rs.next()==false)
+            {
+                password.setText("");
+                wrong_password.setText("Wrong  username or password");
+            }
+            else
+            {
+                Stage primaryStage=new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("user_details.fxml"));
+                primaryStage.initStyle(StageStyle.UTILITY);
+                primaryStage.setTitle("User details");
+                primaryStage.setScene(new Scene(root, 700, 320));
+                primaryStage.show();
+            }
+
         }
         catch(Exception e)
         {
-
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setHeaderText("Account creation unsuccessful");
-
+            errorAlert.setHeaderText("ERROR");
+            errorAlert.setContentText("DB Access error occurred "+e);
+            errorAlert.showAndWait();
         }
     }
+    public void save_to_db(ActionEvent ae)
+    {
+        if(password_create!=password2_create)
+        {
+            password2_create.setText("");
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("ERROR");
+            errorAlert.setContentText("Password mismatch");
+            errorAlert.showAndWait();
+        }
+        else {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mini_project", "root", "password");
+                String sql = "INSERT INTO `user` (`name`, `email`, `password`,`username`) VALUES ( ?, ?, ?,?);";
+                PreparedStatement pr = conn.prepareStatement(sql);
+                pr.setString(1, name_create.getText());
+                pr.setString(2, email_create.getText());
+                pr.setString(3, password_create.getText());
+                pr.setString(4, username_create.getText());
+                pr.executeUpdate();
+            } catch (Exception e) {
+
+                Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+                errorAlert.setHeaderText("ERROR");
+                errorAlert.setContentText("Account creation unsuccessful "+e);
+                errorAlert.showAndWait();
+
+            }
+        }
+    }
+
     public void create()throws Exception
     {
         Stage primaryStage=new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("create_account.fxml"));
-        primaryStage.initStyle(StageStyle.DECORATED);
-        primaryStage.setTitle("User ID System");
+        primaryStage.initStyle(StageStyle.UTILITY);
+        primaryStage.setTitle("Create Account");
         primaryStage.setScene(new Scene(root, 700, 545));
         primaryStage.show();
     }
